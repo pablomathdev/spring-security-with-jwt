e9417466c44e2c8f3741564586662f44b4ff3b10
pablomathdev <pablomatheus171@gmail.com>
2023-12-20 09:42:39 -0400

    First Commit

---
+++ src/main/java/com/github/pablomathdev/login/LoginWebApplication.java
@@ -0,0 +1,13 @@
+package com.github.pablomathdev.login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginWebApplication.class, args);
+	}
+
+}
