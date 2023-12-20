d14d21b959e6143b7982a84976b7f48586ad3c6e
pablomathdev <pablomatheus171@gmail.com>
2023-12-20 10:04:46 -0400

    Added Entity Role

--- src/main/java/com/github/pablomathdev/login/Entities/User.java
+++ src/main/java/com/github/pablomathdev/login/Entities/User.java
@@ -3,6 +3,7 @@ package com.github.pablomathdev.login.Entities;
 import java.time.LocalDate;
 import java.util.Objects;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -18,6 +19,7 @@ public class User {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
+	@Column(unique = true)
 	private Integer cpf;
 	
 	private Integer rg;
