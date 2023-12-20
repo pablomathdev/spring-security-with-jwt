58451921b8aeb0b55dbeeeb0d159f330e7733d52
pablomathdev <pablomatheus171@gmail.com>
2023-12-20 09:55:16 -0400

    added fields cpf and rg in Entity User

--- src/main/java/com/github/pablomathdev/login/Entities/User.java
+++ src/main/java/com/github/pablomathdev/login/Entities/User.java
@@ -1,5 +1,6 @@
 package com.github.pablomathdev.login.Entities;
 
+import java.time.LocalDate;
 import java.util.Objects;
 
 import jakarta.persistence.Entity;
@@ -17,17 +18,35 @@ public class User {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
-	private String name;
+	private Integer cpf;
+	
+	private Integer rg;
+	
+	private String firstName;
+	
+	private String LastName;
 	
 	private String email;
 	
-	private String password;
+	private Integer phone;
 	
-	public User(Long id, String name, String email, String password) {
+	private LocalDate birthDate;
+	
+	private String password;
+
+
+
+	public User(Long id, Integer cpf, Integer rg, String firstName, String lastName, String email, Integer phone,
+			LocalDate birthDate, String password) {
 		super();
 		this.id = id;
-		this.name = name;
+		this.cpf = cpf;
+		this.rg = rg;
+		this.firstName = firstName;
+		LastName = lastName;
 		this.email = email;
+		this.phone = phone;
+		this.birthDate = birthDate;
 		this.password = password;
 	}
 
@@ -39,12 +58,20 @@ public class User {
 		this.id = id;
 	}
 
-	public String getName() {
-		return name;
+	public String getFirstName() {
+		return firstName;
 	}
 
-	public void setName(String name) {
-		this.name = name;
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return LastName;
+	}
+
+	public void setLastName(String lastName) {
+		LastName = lastName;
 	}
 
 	public String getEmail() {
@@ -53,6 +80,22 @@ public class User {
 
 	public void setEmail(String email) {
 		this.email = email;
+	}
+
+	public Integer getPhone() {
+		return phone;
+	}
+
+	public void setPhone(Integer phone) {
+		this.phone = phone;
+	}
+
+	public LocalDate getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(LocalDate birthDate) {
+		this.birthDate = birthDate;
 	}
 
 	public String getPassword() {
@@ -79,7 +122,22 @@ public class User {
 		User other = (User) obj;
 		return Objects.equals(id, other.id);
 	}
-	
-	
+
+	public Integer getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(Integer cpf) {
+		this.cpf = cpf;
+	}
+
+	public Integer getRg() {
+		return rg;
+	}
+
+	public void setRg(Integer rg) {
+		this.rg = rg;
+	}
+
 	
 }
