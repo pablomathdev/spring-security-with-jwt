afe9f536c59105ab5175436aa5ef21d7cd860e8f
pablomathdev <pablomatheus171@gmail.com>
2023-12-21 09:12:39 -0400

    config SecurityFilterChain

---
+++ src/main/java/com/github/pablomathdev/login/Config/WebSecurityConfig.java
@@ -0,0 +1,26 @@
+package com.github.pablomathdev.login.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class WebSecurityConfig {
+    
+	@Bean
+	BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	
+	@Bean
+	SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+
+		http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+	    http.csrf(csrf -> csrf.disable());
+		http.headers(headers -> headers.frameOptions(frameOptionsConfig -> frameOptionsConfig.disable()));
+		return http.build();
+	}
+}
--- src/main/java/com/github/pablomathdev/login/LoginWebApplication.java
+++ src/main/java/com/github/pablomathdev/login/LoginWebApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LoginWebApplication {
+public class LoginWebApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(LoginWebApplication.class, args);
